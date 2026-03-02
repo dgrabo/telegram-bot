1120568bbe780f53960eadc65414348774d46dd8
dgrabo <grabovacdado@gmail.com>
2026-03-02 17:32:28 +0100

    .gitignore and do_HEAD

--- health.py
+++ health.py
@@ -5,6 +5,11 @@ class HealthHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"OK")
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
+        
     def log_message(self, format, *args):
         pass
 
